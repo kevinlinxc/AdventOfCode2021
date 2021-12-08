cae00605a615d6b985b78e9cbdbe2f108f9209d0
Kevin Lin <kevinlinxc@gmail.com>
2021-12-08 00:36:36 -0800

    Day 8 part 1

---
+++ PythonSrc/day8.py
@@ -0,0 +1,12 @@
+file1 = open('inputs\day8.txt', 'r')
+Lines = file1.readlines()
+print(Lines)
+stripped = [(line.strip()) for line in Lines]
+counter = 0
+for line in stripped:
+    right_digits = line.split(' | ')[1].split(" ")
+    for digit in right_digits:
+        if len(digit) in [2, 3, 4, 7]:
+            counter+=1
+print(counter)
+#530
