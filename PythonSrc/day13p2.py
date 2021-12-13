6bf37c67f4acd2b2268d33ced25b7e276b9a2035
Kevin Lin <kevinlinxc@gmail.com>
2021-12-13 01:00:08 -0800

    day 13

---
+++ PythonSrc/day13p2.py
@@ -0,0 +1,68 @@
+file1 = open('inputs\day13.txt', 'r')
+lines = file1.readlines()
+rows = [(line.strip()) for line in lines]
+print(rows)
+import re
+folds = []
+dots = []
+dots_set = set()
+max_x = 0
+max_y = 0
+
+def fold_fun(dots, axis, location):
+    new_dots = set()
+    if axis == "y":
+        for dot in dots:
+            dotx, doty = dot[0], dot[1]
+            if dot[1] > location:
+                new_location = (dotx, location - (doty - location))
+                if new_location[0] >= 0 and new_location[1] >=0:
+                    new_dots.add(new_location)
+            else:
+                new_dots.add(dot)
+        return new_dots
+    if axis == "x":
+        for dot in dots:
+            dotx, doty = dot[0], dot[1]
+            if dot[0] > location:
+                new_location = (location - (dotx - location), doty)
+                if new_location[0] >= 0 and new_location[1] >=0:
+                    #print(f"Mirroring {dotx}, {doty} across y={location} to {new_location}")
+                    new_dots.add(new_location)
+            else:
+                new_dots.add(dot)
+    return new_dots
+
+
+for row in rows:
+    if "fold" in row:
+        print(row)
+        instruction = re.search(r"[a-z]=[0-9]*", row).group(0)
+        # print(f"found fold {instruction}")
+        direction, number = instruction.split("=")
+        folds.append((direction, int(number)))
+    elif "," in row:
+        x,y = row.split(",")
+        x = max(int(x), max_x)
+        y = max(int(y), max_y)
+        dots.append((x,y))
+
+
+# fold = folds[0]
+# board = fold_fun(dots,fold[0], fold[1])
+board= set()
+for fold in folds:
+    print(f"Doing fold: {fold}")
+    board = fold_fun(dots, fold[0], fold[1])
+    dots=board
+    print(f"Board now: {board}")
+    print(f"Board size: {len(board)}")
+# print(dots)
+# print(folds)
+board_list = list(board)
+x = [x[0] for x in board_list]
+y= [y[1] for y in board_list]
+import matplotlib.pyplot as plt
+plt.scatter(x,y)
+plt.show()
+# HZLEHJRK
