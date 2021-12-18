924eea509ebc6ffe5007e2ee9f3b197ceba26cf0
Kevin Lin <kevinlinxc@gmail.com>
2021-12-18 15:28:33 -0800

    pain

--- PythonSrc/day16p2.py
+++ PythonSrc/day16p2.py
@@ -5,11 +5,13 @@ packet = rows[0]
 first_len= len(rows[0])
 # binary = bin(int(rows[0], 16))[2:] # doesn't work for some reason
 binary = ""
+debug = False
 for char in rows[0]:
     new_char = bin(int(char, 16))[2:]
     padded = new_char.zfill(4)
     binary += padded
 # binary = binary.rstrip("0")
+print(packet)
 print(f"Start: Binary is {binary}")
 # lazy enum
 versioning = "V"
@@ -31,72 +33,72 @@ operators = {
 
 def read_packet(binary, number_of_subpackets=None, target_length=None):
     index = 0
-    current = versioning
-    global total_version
-    version = 0
-    id = 0
-    operator = False
-    total_length = 0
     packets_read = 0
 
     while index < len(binary):
+        if debug: print(binary[index:])
         if binary[index:] == len(binary[index:]) * "0":
             if number_of_subpackets or target_length:
                 print("end subpackets")
-                return index, total_version
-            return total_version
-        if current == versioning:
-            start = index
-            three_letters = binary[index:index+3]
-            version = int(three_letters, 2)
+                return index
+        three_letters = binary[index:index+3]
+        version = int(three_letters, 2)
 
-            total_version += version
-            current = iding
-            index += 3
-        elif current == iding:
-            three_letters = binary[index:index+3]
-            id = int(three_letters, 2)
-            if id != 4:
-                mode_string = binary[index+3]
-                index += 4
-                print(f"operator: {operators[id]}")
-                if mode_string == "0":
-                    next_15_bits = binary[index:index+15]
-                    total_length = int(next_15_bits, 2)
-                    index += 15
-                    index_increment, version_bump = read_packet(binary[index:],
-                                                                target_length=total_length)
-                    index += index_increment
-                    total_version += version_bump
-                else:
-                    next_11_bits = binary[index: index+11]
-                    index += 11
-                    number_of_subpackets = int(next_11_bits, 2)
-                    index_increment, version_bump = read_packet(binary[index:], number_of_subpackets=number_of_subpackets)
-                    index += index_increment
-                    total_version += version_bump
+        if debug: print(f"version {version}")
+        index += 3
+        if debug: print(binary[index:])
+        three_letters = binary[index:index+3]
+        id = int(three_letters, 2)
+        if debug: print(f"id: {id}")
+        if id != 4:
+            mode_string = binary[index+3]
+            if debug: print(f"mode: {mode_string}")
+            index += 4
+            if debug: print(binary[index:])
+            print(f"operator: {operators[id]}")
+            if mode_string == "0":
+                next_15_bits = binary[index:index+15]
+                total_length = int(next_15_bits, 2)
+                if debug: print(f"Reading subpacket with length {total_length} from {next_15_bits}")
+                index += 15
+                if debug: print(binary[index:])
+                index_increment = read_packet(binary[index:],
+                                                            target_length=total_length)
+                index += index_increment
+                if debug: print(binary[index:])
             else:
-                index += 3
-                bcd = ""
-                while True:
-                    continues_or_not = int(binary[index])
-                    index += 1
-                    next_4_bits = binary[index: index+4]
-                    bcd = bcd + next_4_bits
-                    index += 4
-                    if not continues_or_not:
-                        break
-                print(f"literal: {int(bcd, 2)}")
-                packets_read += 1
-            if target_length:
-                if index == target_length:
-                    print("end subpackets")
-                    return index, total_version
-            if number_of_subpackets:
-                if packets_read == number_of_subpackets:
-                    print("end subpackets")
-                    return index, total_version
-            current = versioning
+                next_11_bits = binary[index: index+11]
+                number_of_subpackets = int(next_11_bits, 2)
+                if debug: print(f"Reading subpacket with quantity {number_of_subpackets} from {next_11_bits}")
+                index += 11
+                if debug: print(binary[index:])
+
+                index_increment = read_packet(binary[index:], number_of_subpackets=number_of_subpackets)
+                index += index_increment
+                if debug: print(binary[index:])
+        else:
+            index += 3
+            if debug: print(binary[index:])
+            bcd = ""
+            while True:
+                continues_or_not = int(binary[index])
+                index += 1
+                next_4_bits = binary[index: index+4]
+                bcd = bcd + next_4_bits
+                index += 4
+                if debug: print(binary[index:])
+                if not continues_or_not:
+                    break
+            print(f"literal: {int(bcd, 2)} from {bcd}")
+        packets_read += 1
+        if target_length:
+            if index == target_length:
+                print("end subpackets")
+                return index
+        if number_of_subpackets:
+            if packets_read == number_of_subpackets:
+                print("end subpackets")
+                return index
 
 
 read_packet(binary)
--- PythonSrc/day16p2parsing.py
+++ PythonSrc/day16p2parsing.py
@@ -3,15 +3,71 @@ lines = file1.readlines()
 rows = [(line.strip()) for line in lines]
 counter = 0
 from collections import deque
-
-operator_stack = deque()
-operator_count  = 0
+operator_count = 0
 end_count = 0
 for row in rows:
     if "operator" in row:
         operator_count += 1
     elif "end" in row:
         end_count += 1
+print(f"Operators: {operator_count}, ends: {end_count}")
+operator_stack = deque()
+numbers_stack = deque()
+numbers_stack.append([])
+# 0: "sum",
+# 1: "product",
+# 2: "minimum",
+# 3: "maximum",
+# 5: "greater_than",
+# 6: "less_than",
+# 7: "equals"
+for row in rows:
+    if "operator" in row:
+        operator_stack.append(row[len("operator: "):])
+        numbers_stack.append([])
+    elif "literal: " in row:
+        lis1 = numbers_stack.pop()
+        lis1.append(int(row.lstrip("literal: ")))
+        numbers_stack.append(lis1)
+    elif "end subpackets" in row:
+        operator = operator_stack.pop()
+        latest_list = numbers_stack.pop()
+        new_value = 0
+        if operator == "sum":
+            for item in latest_list:
+                new_value += item
+        elif operator == "product":
+            new_value = 1
+            for item in latest_list:
+                new_value *= item
+        elif operator =="minimum":
+            new_value = min(latest_list)
+        elif operator == "maximum":
+            new_value = max(latest_list)
+        elif operator == "greater_than":
+            assert len(latest_list) == 2 ,f"len is {len(latest_list)}"
+            if latest_list[0] > latest_list[1]:
+                new_value = 1
+            else:
+                new_value = 0
+        elif operator == "less_than":
+            assert len(latest_list) == 2, f"len is {len(latest_list)}"
+            if latest_list[0] < latest_list[1]:
+                new_value = 1
+            else:
+                new_value = 0
+        elif operator == "equals":
+            assert len(latest_list) == 2, f"len is {len(latest_list)}"
+            if latest_list[0] == latest_list[1]:
+                new_value = 1
+            else:
+                new_value = 0
 
-print(operator_count)
-print(end_count)
+
+        next_list = numbers_stack.pop()
+        next_list.append(new_value)
+        numbers_stack.append(next_list)
+        if len(list(numbers_stack)) == 1:
+            print(numbers_stack)
+
+# print(numbers_stack)
